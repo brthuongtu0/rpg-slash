237b0c92ac38d1714797556452f6de5194970c40
Thuong Tu <brthuongtu0@gmail.com>
2026-07-07 22:10:59 +0700

    setup header debug macros

---
+++ Source/RPG_Slash/Private/Items/Item.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+#include "RPG_Slash/DebugMacros.h"
+
+AItem::AItem() {
+	PrimaryActorTick.bCanEverTick=true;
+}
+
+void AItem::BeginPlay() {
+	Super::BeginPlay();
+
+	UWorld* World=GetWorld();
+	FVector Location=GetActorLocation();
+	FVector Forward=GetActorForwardVector();
+
+	DRAW_SPHERE(Location);
+	//DRAW_LINE(Location, Location + Forward * 100.f);
+	//DRAW_POINT(Location + Forward * 100.f);
+	DRAW_VECTOR(Location, Location + Forward * 100.f);
+}
+
+void AItem::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
